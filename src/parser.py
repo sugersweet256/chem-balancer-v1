57cf7424f550472f9a41bae235793ba4c14ca83e
sugersweet256 <3636811435@qq.com>
2026-09-05 17:53:42 +0800

    add

--- src/parser.py
+++ src/parser.py
@@ -5,6 +5,7 @@ def parser_info(data: str) -> dict:
     element_list = []
     element_dict = defaultdict(int)
     element_list.append(element_dict)
+    water_time = ''
 
     i = 0
     while i < len(data):
@@ -47,13 +48,35 @@ def parser_info(data: str) -> dict:
             for element, count in inner.items():
                 element_list[-1][element] += count * times
 
+        # 水合物的情况
+        elif ch == '·':
+            # 将其看作括号一样的栈处理
+            element_list.append(defaultdict(int))
+            j = i+1
+            # 获取水合物倍数
+            while j < len(data) and data[j].isdigit():
+                water_time += data[j]
+                j+=1
+
+            if j==i+1:
+                water_time = 1
+            water_time = int(water_time)
+
+            i += 1
+
+
         else:
             # 其它字符（孤立的小写字母、数字等）跳过
             i += 1
+    # 到最后把水合物的部分弹栈再加回第一级栈
+    if len(element_list) > 1:
+        water_part = element_list.pop()
+        for element, count in water_part.items():
+            element_list[-1][element] += count * water_time
 
-    return dict(element_dict)
+    return dict(element_list[0])
 
 
 if __name__ == "__main__":
-    element_dict = parser_info('CH3COOH')
+    element_dict = parser_info('CuSO4·5H2O')
     print(element_dict)
